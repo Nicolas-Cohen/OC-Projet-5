ea0390915f57b15b0ed9e0bb0e44c977286d1903
Nicolas Cohen <nc.cohen@yahoo.com>
2023-04-05 16:39:40 +0200

    first commit

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,53 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let currentSlide= 0;
+const banner = document.querySelector("#banner");
+const bannerImage = banner.children.item(0);
+const bannerTagLine = banner.children.item(1);
+
+document.querySelector(".arrow_left").addEventListener("click", function () {
+	bannerImage.src = "./assets/images/slideshow/" + slides[currentSlide].image ;
+	bannerTagLine.innerHTML = slides[currentSlide].tagLine ;
+	currentSlide-- ;
+	if (currentSlide < 0) {
+		currentSlide = slides.length -1;
+	}
+	bullet(currentSlide);
+});
+
+document.querySelector(".arrow_right").addEventListener("click", function () {
+	bannerImage.src = "./assets/images/slideshow/" + slides[currentSlide].image ;
+	bannerTagLine.innerHTML = slides[currentSlide].tagLine ;	
+	currentSlide++ ;
+	if (currentSlide >= slides.length) {
+		currentSlide = 0;
+	}
+	bullet(currentSlide);
+});
+
+// BULLETS
+const dotContainer = document.querySelector('#js-dots');
+const taille = slides.length - 1;
+
+for (let i = 0; i <= taille; i++) {
+	const newDot = document.createElement('span');
+	newDot.classList.add('dot');
+	dotContainer.appendChild(newDot);
+};
+
+const dots = document.querySelectorAll(".dot");
+
+function bullet(current) {
+    dots.forEach((item, index) => {
+        if (index === current) {
+          item.classList.add('dot_selected');
+        } else {
+          item.classList.remove('dot_selected');
+        }
+    });
+}
+
+bullet(currentSlide);
+console.log(banner.children)
